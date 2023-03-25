dc3be4a249406c148acf2c2f72a9fb3632530f35
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-03-25 10:37:41 +0100

    V1 capacitor configurado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'front',
-  webDir: 'www',
+  appId: 'com.voiceFinder.app',
+  appName: 'Voice-Finder',
+  webDir: 'src',
   bundledWebRuntime: false
 };
 
