d4e00f2f1c45e0c872d63596b8aac9b4ac660d15
Diego Gomez <diego.gomez@cloudappi.net>
2023-04-17 15:18:26 +0200

    Firebase añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import {ComponentsModule} from "./components/components.module";
+import { ComponentsModule } from './components/components.module';
 
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ComponentsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ComponentsModule,
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bd-voicefinder',
+    appId: '1:967565704532:web:bdd7c25c9e5bc6fe27dcc1',
+    databaseURL: 'https://bd-voicefinder-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'bd-voicefinder.appspot.com',
+    apiKey: 'AIzaSyBp2BAu5nzYQg0MfbTkxS-VHVz2enNA1wA',
+    authDomain: 'bd-voicefinder.firebaseapp.com',
+    messagingSenderId: '967565704532',
+  },
   production: false
 };
 
