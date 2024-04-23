f4427c38b2980f511f816b4381fdfe231895066d
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-04-23 21:57:47 +0200

    Fix

---
+++ src/app/components/home/home-my-space/home-my-space.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-my-space',
+  templateUrl: './home-my-space.component.html',
+  styleUrls: ['./home-my-space.component.scss'],
+})
+export class HomeMySpaceComponent implements OnInit {
+  isModalOpen = false;
+
+  //Abrir modal
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+  constructor() {}
+
+  ngOnInit() {}
+}
